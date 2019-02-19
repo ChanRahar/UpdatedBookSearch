f17ea1a0824e8e3d374a539199115a903595884c
ChanRahar <41398292+ChanRahar@users.noreply.github.com>
2019-02-19 16:22:46 -0500

    updated wording for the jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -6,7 +6,7 @@ function Jumbotron() {
     <div className="jumbotron text-center">
       <h1>Google Books Search</h1>
       <a target="_blank" rel="noopener noreferrer" href="https://developers.google.com/books/">
-        Search for and Save Books of Interest 
+        Search and Save Books of Interest 
       </a>
     </div>
   );
